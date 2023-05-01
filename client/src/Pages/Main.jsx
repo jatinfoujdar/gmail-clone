cd1d8485ad7c957a78f55ae9b2566cfff05e5dbe
Jatin Foujdar <jatin321001@gmail.com>
2023-05-01 23:50:43 +0530

    structured components commit

---
+++ client/src/Pages/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Sidebar from '../Components/Sidebar'
+
+const Main = () => {
+  return (
+    <div>
+    <Header/>
+    <Sidebar/>
+    <div>Display Main</div>
+    </div>
+  )
+}
+
+export default Main
