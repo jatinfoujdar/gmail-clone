f8e1285b5b49a1e3abc8bea4baa644812b6ec6f4
Jatin Foujdar <jatin321001@gmail.com>
2023-05-02 18:44:04 +0530

    sidebar structured commit

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Drawer,styled } from '@mui/material';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+   <Drawer anchor='left' open={true} hideBackdrop={true} ModalProps={{keepMounted: true}} variant='persistent' sx={{"& .MuiDrawer-paper":{
+    marginTop: "64px",
+    width: 250,
+    background: "#F5F5F5",
+    borderRight: "none",
+    height: "cal(100vh-64px)"
+   }}}>
+hello
+   </Drawer>
   )
 }
 
